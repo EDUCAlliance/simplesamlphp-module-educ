9cb3b0c7e1a552cbf5f248b130fd4702a6490aed
Uherkovich Péter <uherkovich.peter@gmail.com>
2025-02-13 20:16:22 +0100

    change logo; some layout change; missing styles and fa; welcome page added

--- src/EducThemeController.php
+++ src/EducThemeController.php
@@ -1,7 +1,9 @@
-<?php /** @noinspection PhpIllegalPsrClassPathInspection */
+<?php
+/** @noinspection PhpIllegalPsrClassPathInspection */
 
 namespace SimpleSAML\Module\educ;
 
+use SimpleSAML\Configuration;
 use SimpleSAML\XHTML\Template;
 use SimpleSAML\XHTML\TemplateControllerInterface;
 use Twig\Environment;
@@ -10,18 +12,19 @@ use Twig\Environment;
  * @see https://github.com/simplesamlphp/simplesamlphp/blob/master/docs/simplesamlphp-theming.md
  * @see https://github.com/simplesamlphp/simplesamlphp/wiki
  */
-class EducThemeController implements TemplateControllerInterface {
-    /**
-     * @inheritDoc
-     */
-    public function setUpTwig(Environment &$twig): void {
-        $twig->addGlobal('scope', getenv('SAML_SCOPE')?:'pte.hu');
-        /** @var Template $template */
+class EducThemeController implements TemplateControllerInterface
+{
+    public function setUpTwig(Environment &$twig): void
+    {
     }
 
-    /**
-     * @inheritDoc
-     */
-    public function display(array &$data): void {
+    public function display(array &$data): void
+    {
+    }
+
+    public function welcome(): Template
+    {
+        $config = Configuration::getInstance();
+        return new Template($config, 'educ:welcome.twig');
     }
 }
