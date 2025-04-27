3dd04b7cf15d8023d66ebfcff868275988d0b01f
Uherkovich Péter <uherkovich.peter@gmail.com>
2025-04-27 15:02:29 +0200

    header titles and links

--- src/EducThemeController.php
+++ src/EducThemeController.php
@@ -3,7 +3,9 @@
 
 namespace SimpleSAML\Module\educ;
 
+use Exception;
 use SimpleSAML\Configuration;
+use SimpleSAML\Error\ConfigurationError;
 use SimpleSAML\XHTML\Template;
 use SimpleSAML\XHTML\TemplateControllerInterface;
 use Twig\Environment;
@@ -18,11 +20,21 @@ class EducThemeController implements TemplateControllerInterface
     {
     }
 
+    /**
+     * @throws Exception
+     */
     public function display(array &$data): void
     {
+        $config = Configuration::getInstance();
         $data['env'] = getenv('APPLICATION_ENV')?:'production';
+        $data['homePage'] = $config->getOptionalString('educ.homePage', '/');
+        $data['mainTitle'] = $config->getOptionalString('educ.mainTitle', '');
     }
 
+    /**
+     * @throws ConfigurationError
+     * @throws Exception
+     */
     public function welcome(): Template
     {
         $config = Configuration::getInstance();
