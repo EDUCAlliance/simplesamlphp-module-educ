d78dced3b142c6c4a84f22ff22f8b67516616cab
Uherkovich Péter <uherkovich.peter@gmail.com>
2026-01-23 21:35:06 +0100

    fix SmartEncryption processing filter

--- src/Auth/Process/SmartEncryption.php
+++ src/Auth/Process/SmartEncryption.php
@@ -4,6 +4,7 @@
 namespace SimpleSAML\Module\educ\Auth\Process;
 
 use SimpleSAML\Auth\ProcessingFilter;
+use SimpleSAML\Logger;
 
 /**
  * Switch on encryption if the SP has encryption enabled, except if the SP is in the blacklist.
@@ -27,14 +28,18 @@ class SmartEncryption extends ProcessingFilter {
         if ($spEntityId && in_array($spEntityId, $this->blacklist)) {
             return;
         }
+        Logger::debug("SmartEncryption: SP entity ID: " . $spEntityId);
         // If no keys are defined, don't encrypt.
         if (!isset($state['SPMetadata']['keys'])) {
             return;
         }
+        Logger::debug(json_encode($state['SPMetadata']['keys']));
         // If the SP has encryption enabled, encrypt.
         foreach ($state['SPMetadata']['keys'] as $key) {
             if (isset($key["encryption"]) && $key["encryption"] === true) {
-                $state["saml:EncryptionConfig"]["assertion.encryption"] = true;
+//                $state["saml:EncryptionConfig"]["assertion.encryption"] = true;
+//                $state['Destination']['assertion.encryption'] = true;
+                $state['SPMetadata']['assertion.encryption'] = true;
                 return;
             }
         }
